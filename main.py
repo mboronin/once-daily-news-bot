6cdbfc147f80793a140726cee74e5dca805a0496
Mikhail Boronin <boroninm@gmail.com>
2020-03-30 23:17:52 +0200

    Updates to required packages

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+
 def construct_message(name):
     message = ""
     parser = Parser()
